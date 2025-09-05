8051666113c3ccdc0248127c45d482a864bd4036
Keerthi279 <100715795+Keerthi279@users.noreply.github.com>
2025-09-05 07:57:23 +0530

    Create validation_job.py

---
+++ schema/validation_job.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields, ValidationError, EXCLUDE
+
+
+class ValidationJobSchema(Schema):
+    user_id = fields.Str(required=True)
+    dataset_id = fields.Str(required=True)
+    as_of_date = fields.Str(required=True)
+    table = fields.Str(required=False, allow_none=True)
+    table_group = fields.Str(required=False, allow_none=True)
+
+class ValidationJobSuccessSchema(Schema):
+    status = fields.Str(required=True)
+    message = fields.Str(required=True)
+    user_id = fields.Str(required=True)
+    request_id_received = fields.Str(required=True)
+    data_received = fields.Dict(required=True)
+
